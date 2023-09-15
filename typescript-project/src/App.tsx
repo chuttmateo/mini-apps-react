1b961cec4a067e3f1e4223a72b368aacb11400c4
chuttmateo <chuttmateo0@gmail.com>
2023-09-15 16:28:32 -0300

    feat: 2 class React from 0

--- typescript-project/src/App.tsx
+++ typescript-project/src/App.tsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { Test } from './components'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [a, setA] = useState(0)
+
+  /* 
+  SAMPLE FETCH
+  const fetcher = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    return res.json()
+  }
+
+  const fetchInformation = async () => {
+    const result = await fetcher()
+    setA(result.id)
+  }
+
+  useEffect(() => {
+    fetchInformation()
+  }, []) */
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => setA(a + 1)}> cambiar a</button>
+      <Test><>{a}</></Test>
     </>
   )
 }
